f9a9df43b3785e94da5e5be9b88a55700098b073
Victor3294 <vblum26@gmail.com>
2024-03-20 21:26:52 -0300

    feat (loadUrl): Consumir a API M1S09 exercicio 3

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,22 @@
 
 
 //Victor
-
+const loadUrl =  async () =>{
+    const url = "https://api.api-ninjas.com/v1/exercises?type=stretching";
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {'X-Api-Key' : '8Tos1ttCs+SWR7VVd1lcow==ANHIvSP8R5OEZQCx'}
+        });
+        const data = await response.json();
+        
+        for(let i of data){
+            console.log(i.instructions);
+        }
+    } catch (error) {
+        alert('algo deu errado nos estados')
+    }
+}
 
 
 //Matheus
